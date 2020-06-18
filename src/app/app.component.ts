65f7aded666295cadaa2584176c001ecd33ed405
iury06 <iury.a.r@hotmail.com>
2020-06-17 21:01:33 -0300

    commit, adicionando configurações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login/login.service';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projetoIntegrador-Pi';
+  data: any;
+
+
+   constructor(private login: LoginComponent) {
+
 }
+
+ngOnInit(){
+
+
+}
+
+
+        /*this.data = {
+            labels: ['A','B','C'],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]    
+            };*/
+    }
+
